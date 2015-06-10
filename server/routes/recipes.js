21a7a39569ae8ef9daaa618a68ef2989fc4f167e
Cazanon <carlos.azanon@gmail.com>
2015-06-10 21:13:41 +0200

    s

--- server/routes/recipes.js
+++ server/routes/recipes.js
@@ -1,9 +1,126 @@
-var recipes = [
-    {id:0 , name:"paella", title:"Introduction to Ionic", speaker:"CHRISTOPHE COENRAETS", time:"9:40am", room:"Ballroom A", description: "In this session, you'll learn how to build a native-like mobile application using the Ionic Framework, AngularJS, and Cordova."},
-    {id:1 , name:"espaguetis", title:"AngularJS in 50 Minutes", speaker:"LISA SMITH", time:"10:10am", room:"Ballroom B", description: "In this session, you'll learn everything you need to know to start building next-gen JavaScript applications using AngularJS."},
-    {id:2 , name:"solomillo", title:"Contributing to Apache Cordova", speaker:"JOHN SMITH", time:"11:10am", room:"Ballroom A", description: "In this session, John will tell you all you need to know to start contributing to Apache Cordova and become an Open Source Rock Star."},
-    {id:3 , name:"pollo", title:"Mobile Performance Techniques", speaker:"JESSICA WONG", time:"3:10Pm", room:"Ballroom B", description: "In this session, you will learn performance techniques to speed up your mobile application."},
-    {id:4 , name:"bizcocho", title:"Building Modular Applications", speaker:"LAURA TAYLOR", time:"2:00pm", room:"Ballroom A", description: "Join Laura to learn different approaches to build modular JavaScript applications."}
+var recipes = [    
+    {
+    "id":0,
+    "name": "Paella Mixta",
+    "subname": "Arroz con verduras y pollo todo muy rico!",
+    "category": "Arroces",
+    "ingredients": [
+        {
+       "ingredient": "Pollo",
+       "quantity": "200gr",
+       "details": "Cortado en tacos"
+        }, {
+        "ingredient": "Arroz",
+        "quantity": "500gr",
+        "details": "Arroz bomba"
+        }
+    ],
+    "important": "true",
+    "favourite": [
+        {
+        "favList": "Diario",
+        }, {
+        "favList": "Fines de semana",
+        }
+    ],
+    "step": [
+        {
+        "number": 1,
+        "sumary":"sumary del paso 1",
+        "description": "hay que freir el pollo",
+        "image": "img1"
+        }, {
+        "number": 2,
+        "sumary":"sumary del paso 2",
+        "description": "se añade el arroz",
+        "image": "img2"
+        }
+    ],
+    "image": "../img/fideua.jpg",
+    "icon": "../img/arroz_icon.jpg"
+    },
+    
+    {
+    "id":1,
+    "name": "Paella Mixta",
+    "subname": "Arroz con verduras y pollo todo muy rico!",
+    "category": "Arroces",
+    "ingredients": [
+        {
+       "ingredient": "Pollo",
+       "quantity": "200gr",
+       "details": "Cortado en tacos"
+        }, {
+        "ingredient": "Arroz",
+        "quantity": "500gr",
+        "details": "Arroz bomba"
+        }
+    ],
+    "important": "true",
+    "favourite": [
+        {
+        "favList": "Diario",
+        }, {
+        "favList": "Fines de semana",
+        }
+    ],
+    "step": [
+        {
+        "number": 1,
+        "sumary":"sumary del paso 1",
+        "description": "hay que freir el pollo",
+        "image": "img1"
+        }, {
+        "number": 2,
+        "sumary":"sumary del paso 2",
+        "description": "se añade el arroz",
+        "image": "img2"
+        }
+    ],
+        "image": "../img/fideua.jpg",
+        "icon": "../img/arroz_icon.jpg"
+    },
+    
+    {
+    "id":2,
+    "name": "Paella Mixta",
+    "subname": "Arroz con verduras y pollo todo muy rico!",
+    "category": "Arroces",
+    "ingredients": [
+        {
+       "ingredient": "Pollo",
+       "quantity": "200gr",
+       "details": "Cortado en tacos"
+        }, {
+        "ingredient": "Arroz",
+        "quantity": "500gr",
+        "details": "Arroz bomba"
+        }
+    ],
+    "important": "true",
+    "favourite": [
+        {
+        "favList": "Diario",
+        }, {
+        "favList": "Fines de semana",
+        }
+    ],
+    "step": [
+        {
+        "number": 1,
+        "sumary":"sumary del paso 1",
+        "description": "hay que freir el pollo",
+        "image": "img1"
+        }, {
+        "number": 2,
+        "sumary":"sumary del paso 2",
+        "description": "se añade el arroz",
+        "image": "img2"
+        }
+    ],    
+        "image": "../img/fideua.jpg",
+        "icon": "../img/arroz_icon.jpg"
+    }
 ];
 
 exports.findAll = function (req, res, next) {
